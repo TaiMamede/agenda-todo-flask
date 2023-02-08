e43203d0d0711bc78c8d87bfd5b16dd3136a2110
devtiagomantay <devtiagomantay@gmail.com>
2023-02-08 22:14:36 +0000

    Move html file to static folder and return it on main endpoint

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Good'
+    return render_template('index.html')
