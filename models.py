fa5eb5e69741d7d18a95aa38f1b9f321f933221d
tainamamederibeiro@gmail.com <tainamamederibeiro@gmail.com>
2023-03-06 22:50:03 +0000

    new class

--- models.py
+++ models.py
@@ -18,5 +18,35 @@ class Task(db.Model):
 		self.guest = guest,
 		self.deadline = deadline
 
+class user(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(120))
+	email = db.Column(db.String(120))
+	created = db.Column(db.DateTime)
+	bio = db.Column(db.String(500))
 
-# TODO: classe user e event
+	def __init__(self, name, email, created=None, bio=None):
+		self.name = name,
+		self.email = email,
+		self.created = created,
+		self.bio = bio,
+
+class event(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(120))
+	user_id = db.Column(db.Integer)
+	guest = db.Column(db.String(120))
+	date = db.Column(db.DateTime)
+	obs = db.Column(db.String(500))
+	created = db.Column(db.DateTime(120))
+
+	def __init__(self, id, title, user_id, guest=None, date=None, obs=None, created=None):
+		self.id = id,
+		self.title = title
+		self.user_id =	user_id
+		self.guest = guest
+		self.date = date
+		self.obs = obs
+		self.created = created
+
+
